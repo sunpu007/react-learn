8ef79f89dc5ff9e12318be6c02dc135b7ee6feab
Jerry <731238433@qq.com>
2019-09-04 17:30:35 +0800

    'useEffect'

--- demo06/src/Example.js
+++ demo06/src/Example.js
@@ -1,14 +1,43 @@
 import React, { Fragment, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+function Index () {
+  useEffect(() => {
+    console.log('老弟，你来了，Index')
+    return () => {
+      console.log('老弟，你走了，Index')
+    }
+  }, [])
+  return <h2>Index</h2>
+}
+
+function List () {
+  useEffect(() => {
+    console.log('老弟，你来了，List')
+  })
+  return <h2>List</h2>
+}
 
 function Example () {
   const [count, setCount] = useState(0)
   useEffect(() => {
     console.log(`useEffect=>You clicked ${count} times`)
-  })
+    return () => {
+      console.log('++++++++++++++++++++++++')
+    }
+  }, [count])
   return (
     <Fragment>
       <p>you clicked {count} times</p>
       <button onClick={()=>{setCount(count+1)}}>click me</button>
+      <Router>
+        <ul>
+          <li><Link to='/'>index</Link></li>
+          <li><Link to='/list/'>list</Link></li>
+        </ul>
+        <Route path='/' exact component={Index} />
+        <Route path='/list/' component={List} />
+      </Router>
     </Fragment>
   )
 }
